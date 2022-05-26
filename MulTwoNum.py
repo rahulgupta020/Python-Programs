b53d1169f81aa64f910d2ccaed3a92c50278d051
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-05-26 15:18:31 +0530

    Create MulTwoNum.py

---
+++ MulTwoNum.py
@@ -0,0 +1,11 @@
+"""
+Multiplication of Two Numbers in Python
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+result = num1 * num2
+print("Multiplication of two number is :- ",result)
