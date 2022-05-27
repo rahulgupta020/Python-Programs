96a89a0fd566659bd849d5e45e4669f8a4ec3650
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-05-27 17:35:33 +0530

    Create AdditionSubtractionMultiplicationDivisionOfTwoNum.py

---
+++ AdditionSubtractionMultiplicationDivisionOfTwoNum.py
@@ -0,0 +1,20 @@
+"""
+Python Program – Addition, Subtraction, Multiplication & Division
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+add = num1+num2
+print("Addition = ",add)
+
+sub = num1-num2
+print("Subtraction = ",sub)
+
+mul = num1*num2
+print("Multiplication = ",mul)
+
+div = num1//num2
+print("Division = ",div)
