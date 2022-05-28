d1a9089042a6400df478a777ad8e9ea0f30498d3
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-05-28 16:38:07 +0530

    Create TakeUserInput.py

---
+++ TakeUserInput.py
@@ -0,0 +1,22 @@
+"""
+Take user input in python
+
+Code:
+"""
+
+name = input("Enter your name :- ")
+print("My name is = ",name)
+
+num = int(input("Enter a number :- "))
+add = num+1
+print("Addition = ",add)
+
+num = float(input("Enter a number :- "))
+add = num+1
+print("Addition = ", add)
+
+lst1 = list(input("Enter a list value :- "))
+print("List Values are = ", lst1)
+
+tuple1 = tuple(input("Enter a tuple value :- "))
+print("Tuple Values are = ", tuple1)
