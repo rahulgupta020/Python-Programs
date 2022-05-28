69bf77f696f51d5ca0dfdd986aed4665a32c0fba
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-05-28 18:11:57 +0530

    Create CheckVariableDataType.py

---
+++ CheckVariableDataType.py
@@ -0,0 +1,15 @@
+"""
+Check variable data types in Python
+
+Code:
+"""
+
+string_var = "Rahul Gupta"
+integer_var = 10
+float_var = 10.5
+list_var = [1,2,3,4,5]
+
+print(type(string_var))
+print(type(integer_var))
+print(type(float_var))
+print(type(list_var))
