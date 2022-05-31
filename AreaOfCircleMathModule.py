adc137050f7e67a41170b08eeabefca6d43db32e
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-05-31 14:31:46 +0530

    Create AreaOfCircleMathModule.py

---
+++ AreaOfCircleMathModule.py
@@ -0,0 +1,11 @@
+"""
+How to Calculate the Area of the Circle using Math Module in Python.
+
+Code:
+"""
+
+import math
+
+r = float(input("Enter a radius :- "))
+area = math.pi * r*r
+print("Area of Circle = %.3f" %area)
