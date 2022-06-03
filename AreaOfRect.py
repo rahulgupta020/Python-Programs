b2f54777c49ebe1c96ce1e3a918057f0f8cf3687
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-06-03 13:48:41 +0530

    Create AreaOfRect.py

---
+++ AreaOfRect.py
@@ -0,0 +1,11 @@
+"""
+Python program to find an area of a rectangle.
+
+Code:
+"""
+
+length = int(input("Enter the length :- "))
+bredth = int(input("Enter the bredth :- "))
+
+area = length*bredth
+print("Area of Rectangle = ", area)
