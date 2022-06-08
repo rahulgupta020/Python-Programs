2d8bfda3fc2b812c676c2c53e4873e67287eec01
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-06-08 15:20:53 +0530

    Create PerimeterOfRect.py

---
+++ PerimeterOfRect.py
@@ -0,0 +1,11 @@
+"""
+Python Program to find the perimeter of the rectangle.
+
+Code:
+"""
+
+length = int(input("Enter the length :- "))
+bredth = int(input("Enter the bredth :- "))
+
+perimeter = 2*(length+bredth)
+print("Perimeter of Rectangle = ",perimeter)
