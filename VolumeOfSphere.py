91f7969498797d4ec0703140c46fecbebee26c7d
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-06-21 13:02:28 +0530

    Create VolumeOfSphere.py

---
+++ VolumeOfSphere.py
@@ -0,0 +1,11 @@
+"""
+Python Program to find Volume of sphere.
+
+Code:
+"""
+
+import math
+r = int(input("Enter Radius :- "))
+
+result = (4/3) * math.pi * (r**3)
+print("The Volume of sphere = ", result)
