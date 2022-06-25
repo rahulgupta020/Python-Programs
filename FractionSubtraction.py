02c9f55737345b35da34e29bd8cef3dc3d25f678
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-06-25 20:18:54 +0530

    Create FractionSubtraction.py

---
+++ FractionSubtraction.py
@@ -0,0 +1,16 @@
+"""
+Subtraction of two fractions in python.
+
+Code:
+"""
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+result1 = (A*D) - (C*B)
+result2 = (B*D)
+
+print("The result = ", result1/result2)
+print("The result = {}/{}".format(result1, result2))
