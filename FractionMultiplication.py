c797a884112796a6dbb38138312f3581c72d5b6b
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-06-29 13:35:26 +0530

    Create FractionMultiplication.py

---
+++ FractionMultiplication.py
@@ -0,0 +1,16 @@
+"""
+Multiplication of two fractions in python.
+
+Code:
+"""
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+result1 = A*C
+result2 = B*D
+
+print("The Result = ", result1/result2)
+print("The Result = {}/{}".format(result1, result2))
