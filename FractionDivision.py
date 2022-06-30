56e248acec5b57b9f66034d58431d1acbc0e343d
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-06-30 13:41:30 +0530

    Create FractionDivision.py

---
+++ FractionDivision.py
@@ -0,0 +1,16 @@
+"""
+Division of two fractions in python.
+
+Code:
+"""
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+result1 = A*D
+result2 = B*C
+
+print("The Result = ", result1/result2)
+print("The Result = {}/{}".format(result1, result2))
