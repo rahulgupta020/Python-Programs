74d8ba8f92da3aeecfb55456d4e9672c2536143e
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-07-02 12:48:15 +0530

    Create AlgebricFormula1.py

---
+++ AlgebricFormula1.py
@@ -0,0 +1,10 @@
+"""
+Python program to find algebric formula i.e (a+b)2 =a2 + b2 + 2ab
+
+Code:
+"""
+a = int(input("Enter A Value :- "))
+b = int(input("Enter B Value :- "))
+
+result = (a*a) + (b*b) + (2*a*b)
+print("The result = ", result)
