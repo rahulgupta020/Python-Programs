21c6411e85d1963dac99ba0ad7589acbe34fc7bc
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-07-10 11:58:04 +0530

    Create AlgebricFormula4.py

---
+++ AlgebricFormula4.py
@@ -0,0 +1,12 @@
+"""
+Python program to find algebric formula i.e (a2-b2) = (a+b)(a-b)
+
+Code:
+"""
+
+
+a = int(input("Enter A Value :- "))
+b = int(input("Enter B Value :- "))
+
+result1 = (a+b)*(a-b)
+print("The Result = ",result1)
