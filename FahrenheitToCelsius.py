f479680bcf4bfb9b656e960a9c6e817723c7d0a2
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-07-24 22:05:07 +0530

    Create FahrenheitToCelsius.py

---
+++ FahrenheitToCelsius.py
@@ -0,0 +1,9 @@
+"""
+Program to convert Fahrenheit into Celsius.
+
+Code:
+"""
+
+fah = float(input("Enter Fahrenheit value :- "))
+celsius = (fah-32)*(5/9)
+print("Fahrenheit To Celsius = ",celsius)
