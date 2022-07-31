c4d44407648981a982b51950ec974a67cd18b97b
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-07-31 13:27:03 +0530

    Create SwapOfTwoNumberWithoutTempXorOperator.py

---
+++ SwapOfTwoNumberWithoutTempXorOperator.py
@@ -0,0 +1,16 @@
+"""
+Write a python program to swap of two number without using third variable or temp variable.
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+num1 = num1^num2
+num2 = num1^num2
+num1 = num1^num2
+
+print("\nAfter Swapping\n")
+print("Num1 = ", num1)
+print("Num2 = ", num2)
