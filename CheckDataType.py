0d15c796caff0fbf5ff41ad81e8ecd6f521123fa
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-08-02 14:02:13 +0530

    Create CheckDataType.py

---
+++ CheckDataType.py
@@ -0,0 +1,19 @@
+"""
+Python program to type of data in python.
+
+Code:
+"""
+
+rollno = 1
+name = "Rahul Gupta"
+marks = 99.99
+lst = [1,2,3]
+tup = (1,2,3)
+dict1 = {"name": "Rahul"}
+
+print(type(rollno))
+print(type(name))
+print(type(marks))
+print(type(lst))
+print(type(tup))
+print(type(dict1))
