35a6b935f4f7b00638e489d4701ff261038daf96
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-08-08 18:17:07 +0530

    Create EvenOdd.py

---
+++ EvenOdd.py
@@ -0,0 +1,13 @@
+"""
+Write a python program to find number is even or odd.
+
+Code:
+"""
+n = int(input("Enter a number :- "))
+if(n>0):
+    if(n%2==0):
+        print("Even")
+    else:
+        print("Odd")
+else:
+    print("Invalid")
