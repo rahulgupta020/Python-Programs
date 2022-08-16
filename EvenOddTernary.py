fe709876498fe1f87321e5a135e08d24e2499681
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-08-16 19:03:26 +0530

    Create EvenOddTernary.py

---
+++ EvenOddTernary.py
@@ -0,0 +1,8 @@
+"""
+How can I check whether a number is odd or even without using if/else or the ternary operator in Python?
+
+Code:
+"""
+
+n = int(input("Enter a number :- "))
+print("Even") if(n%2==0) else print("Odd")
