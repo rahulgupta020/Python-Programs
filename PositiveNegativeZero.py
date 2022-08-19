a25f03b10584e7246058982811cda828b8215c4f
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-08-19 15:03:21 +0530

    Create PositiveNegativeZero.py

---
+++ PositiveNegativeZero.py
@@ -0,0 +1,15 @@
+"""
+Python Program to Check Whether a Number is Positive or Negative or zero
+
+Code:
+"""
+
+num = int(input("Enter a number :- "))
+if(num==0):
+    print("Number is zero")
+elif(num>0):
+    print("Number is Positive")
+elif(num<0):
+    print("Number is Negative")
+else:
+    print("Number is Invalid")
