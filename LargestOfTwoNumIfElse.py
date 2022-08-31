fff890152e79578bf61b2d8e9e9d6fa86f078e83
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-08-31 15:20:18 +0530

    Create LargestOfTwoNumIfElse.py

---
+++ LargestOfTwoNumIfElse.py
@@ -0,0 +1,15 @@
+"""
+Largest of two number in python
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+if(num1>num2):
+    print(num1, " is Greater")
+elif(num2>num1):
+    print(num2, " is Greater")
+else:
+    print("Both numbers are equal")
