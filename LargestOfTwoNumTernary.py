54e6f25f23dad63de491e652d9fb21fb4ac90dbd
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-09-01 13:31:13 +0530

    Create LargestOfTwoNumTernary.py

---
+++ LargestOfTwoNumTernary.py
@@ -0,0 +1,10 @@
+"""
+Python Program To Find The Largest Among Two Number Using Ternary Operator
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+print(num1 if(num1>=num2) else num2)
