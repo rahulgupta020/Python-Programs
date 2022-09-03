c0e7715fb1b0870bc254b6e8a0db65415eca4825
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-09-03 14:45:13 +0530

    Create LargestOfTwoNumMax.py

---
+++ LargestOfTwoNumMax.py
@@ -0,0 +1,11 @@
+"""
+Python Program To Find The Largest Among Two Number Using Max Function.
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+result = max(num1, num2)
+print(result, " is Greater")
