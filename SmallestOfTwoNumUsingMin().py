70d0aea6732d7254933b08cc245efc63eb4d70b0
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-09-04 13:39:17 +0530

    Create SmallestOfTwoNumUsingMin().py

---
+++ SmallestOfTwoNumUsingMin().py
@@ -0,0 +1,11 @@
+"""
+Python Program To Find The Smallest Among Two Number using min() function.
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+
+result = min(num1, num2)
+print(result, " is Smaller")
