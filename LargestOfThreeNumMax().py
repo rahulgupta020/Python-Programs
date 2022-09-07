3d962df13f47bb3c3b30a79ac1941f5cd14b3b9e
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-09-07 13:46:58 +0530

    Create LargestOfThreeNumMax().py

---
+++ LargestOfThreeNumMax().py
@@ -0,0 +1,12 @@
+"""
+Python – Largest of Three Numbers using max() function
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+num3 = int(input("Enter num3 :- "))
+
+result = max(num1, num2, num3)
+print(result, "is Greater")
