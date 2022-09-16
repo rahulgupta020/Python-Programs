ed78e3adb43459e3031e2e8858e36c1be218eecf
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-09-16 13:34:45 +0530

    Create LargestOfFourNumMax.py

---
+++ LargestOfFourNumMax.py
@@ -0,0 +1,13 @@
+"""
+Python – Largest of four Numbers using max function
+
+Code:
+"""
+
+num1 = int(input("Enter num1 :- "))
+num2 = int(input("Enter num2 :- "))
+num3 = int(input("Enter num3 :- "))
+num4 = int(input("Enter num4 :- "))
+
+result = max(num1, num2, num3, num4)
+print(result, " is Greater")
