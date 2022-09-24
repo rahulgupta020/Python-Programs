38008d65c4616fb9bf6cc95ab21d3108f27ce8ba
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-09-24 23:01:21 +0530

    Create SumOfNNaturalNum2.py

---
+++ SumOfNNaturalNum2.py
@@ -0,0 +1,10 @@
+"""
+Sum of First N Natural Numbers using Formula in Python
+
+Code:
+"""
+
+n = int(input("Enter N Value :- "))
+
+result = n*(n+1)/2
+print("Sum of n natural number = ", int(result))
