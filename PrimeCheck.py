febe0fb074f61c3d0373ed1e003085fec1d63716
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-10-04 15:23:45 +0530

    Create PrimeCheck.py

---
+++ PrimeCheck.py
@@ -0,0 +1,14 @@
+"""
+Python program to check given number is prime or not.
+
+Code:
+"""
+
+n = int(input("Enter a number :- "))
+
+for i in range(2, n):
+    if(n%i==0):
+        print(n," Not Prime")
+        break
+else:
+    print(n," Prime")
