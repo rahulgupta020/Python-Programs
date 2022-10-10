db2a7a0987c59a21f0bed2cd67177152760efa85
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-10-10 12:53:13 +0530

    Create ReverseNumberSlicing.py

---
+++ ReverseNumberSlicing.py
@@ -0,0 +1,10 @@
+"""
+Write a program to reverse digits of a number using String Slice
+
+Code:
+"""
+
+num =  int(input("Enter a number :- "))
+
+revnum = str(num)[::-1]
+print("Reversed Number = ", revnum)
