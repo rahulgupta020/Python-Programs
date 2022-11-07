62361b8c7c80286542bb7a96fc75d0c9ebbacd8e
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-11-07 10:53:18 +0530

    Create AlphabetCheck.py
    Character is alphabet or not in python

---
+++ AlphabetCheck.py
@@ -0,0 +1,12 @@
+"""
+Character is alphabet or not in python.
+
+Code:
+"""
+
+ch = input("Enter a Character :- ")
+
+if(ch>='a' and ch<='z') or (ch>='A' and ch<='Z'):
+    print(ch, " is Alphabet")
+else:
+    print(ch, " is not alphabet")
