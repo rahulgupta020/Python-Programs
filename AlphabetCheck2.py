03f07f5f2a454bf1bf3170a0328d94b17adaae3b
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-11-09 09:32:18 +0530

    Create AlphabetCheck2.py
    Character is alphabet or not using isalpha() in python.

---
+++ AlphabetCheck2.py
@@ -0,0 +1,12 @@
+"""
+Character is alphabet or not using isalpha() in python.
+
+Code:
+"""
+
+ch = input("Enter a character :- ")
+
+if(ch.isalpha()):
+    print(ch, " is Alphabet")
+else:
+    print(ch, " is not Alphabet")
