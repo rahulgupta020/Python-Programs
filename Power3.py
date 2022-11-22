006532c9c2b392b5a0ebe979286bb0b9ea593b22
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-11-22 13:15:48 +0530

    Create Power3.py

---
+++ Power3.py
@@ -0,0 +1,11 @@
+"""
+Python Program to find Power of a number using math.pow() function
+
+Code:
+"""
+
+base = int(input("Enter Base :- "))
+exp = int(input("Enter Exponent :- "))
+
+result = pow(base, exp)
+print("Power = ", result)
