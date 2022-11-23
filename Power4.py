96afc529f089a9337293fab3a80cb0cd695eb3c7
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-11-23 13:43:57 +0530

    Create Power4.py

---
+++ Power4.py
@@ -0,0 +1,11 @@
+"""
+Python Program to find Power of a number using exponent operator
+
+Code:
+"""
+
+base = int(input("Enter Base :- "))
+exp = int(input("Enter Exponent :- "))
+
+result = base**exp
+print("Power = ", result)
