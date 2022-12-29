c66315965eb332df429eaaf675f190ae90034cff
Rahul Gupta <42673573+rahulgupta020@users.noreply.github.com>
2022-12-29 09:09:30 +0530

    Create Vote.py

---
+++ Vote.py
@@ -0,0 +1,12 @@
+"""
+Python tutorial to check if a user is eligible for voting or not
+
+Code:
+"""
+
+age = int(input("Enter Age :- "))
+
+if(age>=18):
+    print("Eligible for Vote")
+else:
+    print("Not Eligible for Vote")
